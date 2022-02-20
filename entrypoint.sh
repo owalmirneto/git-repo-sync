e313bc76c9a2283f38220d3f47a46bd0cb6bc7cf
owalmirneto <owalmirneto@gmail.com>
2022-02-19 21:07:51 -0300

    feat: change var names

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
-git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
+CREDENTIALS="${TARGET_USERNAME}:${TARGET_TOKEN}";
+REPO_HOST="${TARGET_DOMAIN}/${TARGET_USERNAME}/${TARGET_REPOSITORY}";
 
-case "${GITHUB_EVENT_NAME}" in
-    push)
-        git push -f --all target
-        git push -f --tags target
-        ;;
-    delete)
-        git push -d target ${GITHUB_EVENT_REF}
-        ;;
-    *)
-        break
-        ;;
+git remote add target https://${CREDENTIALS}@${REPO_HOST}.git;
+
+case $GITHUB_EVENT_NAME in
+  push)
+    git push -f --all target && git push -f --tags target
+    ;;
+  delete)
+    git push -d target ${GITHUB_CURRENT_BRANCH};
+    ;;
+  *)
+    break
+    ;;
 esac
